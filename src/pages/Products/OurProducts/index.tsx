19f54bb41b48dd25cf68c9e5ad55f92ff7e2eea6
Muhammad Talha <muhammadtalha517@gmail.com>
2023-12-29 01:27:30 +0500

    Option to update previous rating / starred product

--- src/pages/Products/OurProducts/index.tsx
+++ src/pages/Products/OurProducts/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import axios from "axios";
 import { Star, WalletIcon } from "lucide-react";
-import React from "react";
-import StarredDialogue from "../StarredDialogue";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
 
 interface Product {
   name: string;
@@ -57,15 +59,73 @@ interface ProductCardProps {
   dialogRef: React.RefObject<HTMLDialogElement>;
 }
 
+interface StarredProduct {
+  user_name: string;
+  user_email: string;
+  product_name: string;
+  product_rating: number;
+  id?: number;
+}
+
 const ProductCard: React.FC<ProductCardProps> = ({
   product,
   setStarredProduct,
   dialogRef,
 }) => {
   const [hoveredStar, setHoveredStar] = React.useState<number>(0);
+  const [starred, setStarred] = React.useState<StarredProduct>({
+    user_name: "",
+    user_email: "",
+    product_name: "",
+    product_rating: 0,
+    id: 0,
+  });
+  useEffect(() => {
+    const starredProducts =
+      JSON.parse(localStorage.getItem("starredProducts") || "[]") || [];
+
+    const isStarred = starredProducts.find(
+      (starredProduct: StarredProduct) =>
+        starredProduct.product_name === product.name
+    );
+    if (isStarred) {
+      setStarred(isStarred);
+    }
+  }, [product, localStorage.getItem("starredProducts")]);
+  const handleStarClicked = async (index: number) => {
+    const starredProducts = {
+      ...product,
+      rating: index + 1,
+    };
+    setStarredProduct(starredProducts);
+    if (starred.product_rating !== 0 && starred.id) {
+      await axios.patch(`http://localhost:3000/starred/${starred.id}`, {
+        product_rating: index + 1,
+      });
+      const prevLocalStorage =
+        JSON.parse(localStorage.getItem("starredProducts") || "[]") || [];
+      const parsedLocalStorage = prevLocalStorage.map(
+        (item: StarredProduct) => {
+          if (item.id === starred.id) {
+            return { ...item, product_rating: index + 1 };
+          }
+          return item;
+        }
+      );
+      localStorage.setItem(
+        "starredProducts",
+        JSON.stringify(parsedLocalStorage)
+      );
+      toast.success("Product Rating Updated Successfully");
+    } else {
+      dialogRef.current?.showModal();
+    }
+  };
+
+  const preFill = starred.product_rating;
 
   return (
-    <Card className="w-[30%] min-w-[372px] p-6 m-3 rounded-none shadow-none hover:border-[#942D3B] hover:border hover:border-dashed bg-[#FAF4F3]">
+    <Card className="w-[30%] min-w-[372px] px-6 pt-5 pb-1 m-3 rounded-none shadow-none hover:border-[#942D3B] hover:border hover:border-dashed bg-[#FAF4F3]">
       <CardContent>
         <h3 className="font-[Lato] font-bold text-[16px]">{product.name}</h3>
         <p className="py-3 text-justify font-[Lato] text-[14px]">
@@ -89,21 +149,25 @@ const ProductCard: React.FC<ProductCardProps> = ({
                 key={index}
                 size={20}
                 className="text-[#E3EBF3] hover:text-[#F5A623] "
-                fill={index + 1 <= hoveredStar ? "#F5A623" : "#E3EBF3"}
+                fill={
+                  hoveredStar > 0
+                    ? index + 1 <= hoveredStar
+                      ? "#F5A623"
+                      : "#E3EBF3"
+                    : index + 1 < preFill
+                    ? "#F5A623"
+                    : "#E3EBF3"
+                }
                 onMouseEnter={() => setHoveredStar(index + 1)}
                 onMouseLeave={() => setHoveredStar(0)}
-                onClick={() => {
-                  setStarredProduct({
-                    ...product,
-                    rating: index + 1,
-                  });
-
-                  dialogRef.current?.showModal();
-                }}
+                onClick={() => handleStarClicked(index + 1)}
               />
             ))}
           </div>
         </div>
+        <Button className="mt-4 -mb-4 bg-[#F8F1EF] text-[#942D3B] border-[#942D3B] border hover:bg-[#942D3B] hover:text-[#F8F1EF] transition duration-500 ease-in-out">
+          Show Details
+        </Button>
       </CardContent>
     </Card>
   );
