eaba2484b7fa5a6bcbd7abcf68e225e37c3b96b7
Muhammad Talha <muhammadtalha517@gmail.com>
2023-12-28 20:51:09 +0500

    Refactor OurProducts component to use ProductCard component Star hover improved

--- src/pages/Products/OurProducts/index.tsx
+++ src/pages/Products/OurProducts/index.tsx
@@ -39,54 +39,73 @@ const OurProducts: React.FC<OurProductsProps> = ({
       <div className="flex w-full justify-center flex-wrap pt-4">
         {Array.isArray(products) &&
           products.map((product, index) => (
-            <Card
+            <ProductCard
               key={index}
-              className="w-[30%] min-w-[372px] p-6 m-3 rounded-none shadow-none hover:border-[#942D3B] hover:border hover:border-dashed bg-[#FAF4F3]"
-            >
-              <CardContent>
-                <h3 className="font-[Lato] font-bold text-[16px]">
-                  {product.name}
-                </h3>
-                <p className="py-3 text-justify font-[Lato] text-[14px]">
-                  {product.description}
-                </p>
-                <hr className="" />
-
-                <div className="flex justify-between pt-2">
-                  <div className="flex">
-                    <WalletIcon className="mr-2" />
-                    <p className="font-[Lato] font-bold text-[15px]">
-                      {product.price.toLocaleString("en-US", {
-                        style: "currency",
-                        currency: "USD",
-                      })}
-                    </p>
-                  </div>
-                  <div className="flex">
-                    {Array.from({ length: 5 }, (_, index) => (
-                      <Star
-                        key={index}
-                        size={20}
-                        className="text-[#E3EBF3] hover:text-[#F5A623] "
-                        fill="#E3EBF3"
-                        onClick={() => {
-                          setStarredProduct({
-                            ...product,
-                            rating: index + 1,
-                          });
-
-                          dialogRef.current?.showModal();
-                        }}
-                      />
-                    ))}
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
+              product={product}
+              dialogRef={dialogRef}
+              setStarredProduct={setStarredProduct}
+            />
           ))}
       </div>
     </div>
   );
 };
 
+interface ProductCardProps {
+  product: Product;
+  setStarredProduct: React.Dispatch<React.SetStateAction<Starred>>;
+  dialogRef: React.RefObject<HTMLDialogElement>;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({
+  product,
+  setStarredProduct,
+  dialogRef,
+}) => {
+  const [hoveredStar, setHoveredStar] = React.useState<number>(0);
+
+  return (
+    <Card className="w-[30%] min-w-[372px] p-6 m-3 rounded-none shadow-none hover:border-[#942D3B] hover:border hover:border-dashed bg-[#FAF4F3]">
+      <CardContent>
+        <h3 className="font-[Lato] font-bold text-[16px]">{product.name}</h3>
+        <p className="py-3 text-justify font-[Lato] text-[14px]">
+          {product.description}
+        </p>
+        <hr className="" />
+
+        <div className="flex justify-between pt-2">
+          <div className="flex">
+            <WalletIcon className="mr-2" />
+            <p className="font-[Lato] font-bold text-[15px]">
+              {product.price.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}
+            </p>
+          </div>
+          <div className="flex">
+            {Array.from({ length: 5 }, (_, index) => (
+              <Star
+                key={index}
+                size={20}
+                className="text-[#E3EBF3] hover:text-[#F5A623] "
+                fill={index + 1 <= hoveredStar ? "#F5A623" : "#E3EBF3"}
+                onMouseEnter={() => setHoveredStar(index + 1)}
+                onMouseLeave={() => setHoveredStar(0)}
+                onClick={() => {
+                  setStarredProduct({
+                    ...product,
+                    rating: index + 1,
+                  });
+
+                  dialogRef.current?.showModal();
+                }}
+              />
+            ))}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 export default OurProducts;
