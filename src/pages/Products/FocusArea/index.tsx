2922d4685485f9cb1350818f5c2efb8f8d7695c3
Muhammad Talha <muhammadtalha517@gmail.com>
2023-12-29 14:15:23 +0500

    Update FocusArea component styling

--- src/pages/Products/FocusArea/index.tsx
+++ src/pages/Products/FocusArea/index.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const FocusArea: React.FC = () => {
   return (
-    <div className="relative h-full">
+    <div className="relative h-full min-h-40">
       <div className="absolute w-full z-10 flex flex-col text-center items-center h-full justify-center -mt-4">
-        <h1 className="font-[Montserrat] md:text-[58px]  text-[white] pb-0 mb-0 uppercase">
+        <h1 className="font-[Montserrat] sm:text-[30px] lg:text-[58px]  text-[white] pb-0 mb-0 uppercase">
           Solutions that Inspire,
         </h1>
-        <h1 className="font-[Montserrat] md:text-[58px] text-[white] pt-0 -mt-4 uppercase mb-20">
+        <h1 className="font-[Montserrat] sm:text-[30px] lg:text-[58px] text-[white] pt-0 -mt-4 uppercase mb-20">
           Products that Deliver
         </h1>
-        <p className="font-[Lato] md:text-[15px] text-[white] max-w-[1000px]">
+        <p className="font-[Lato] text-[15px] text-[white] max-w-[1000px]">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
           tellus neque, malesuada sit amet auctor ac, euismod sed enim. Class
           aptent taciti sociosqu ad litora torquent per conubia nostra, per
